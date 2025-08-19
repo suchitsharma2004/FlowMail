4ca851036b39730ab6a7347b9c5fc8232013967e
suchitsharma2004 <suchit.sharma.delhi@gmail.com>
2025-08-19 12:40:04 +0530

    Fix deployment: Add PYTHONPATH, upgrade pip, specify dependency versions

--- MailProject/build.sh
+++ MailProject/build.sh
@@ -6,7 +6,14 @@ set -o errexit
 cd "$(dirname "$0")"
 
 # Install dependencies
+pip install --upgrade pip
 pip install -r ../requirements.txt
+
+# Set Django settings module
+export DJANGO_SETTINGS_MODULE=MailProject.settings
+
+# Add current directory to Python path
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
 # Collect static files
 python manage.py collectstatic --no-input
